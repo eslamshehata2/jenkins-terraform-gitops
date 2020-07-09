eb135caafa5e327f49575a28a4e14d7cba8ccc38
Islam Shehata <eslamshehata476@gmail.com>
2020-07-09 12:52:28 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "instance_count" {
 
 variable "key_name" {
   description = "Private key name to use with instance"
-  default     = "test"
+  default     = "eslam"
 }
 
 variable "instance_type" {
